34f6ac7bbeb311a46a0ce540de53261cd634fbe6
voidcoffee <foredegraceleaff@gmail.com>
2024-05-30 15:47:08 +0700

    test

--- resources/views/admin/DataCustomer.blade.php
+++ resources/views/admin/DataCustomer.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <div class="stats shadow flex justify-center items-center px-10">
-  
+ test
   <div class="stat">
     <div class="stat-figure text-secondary">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
